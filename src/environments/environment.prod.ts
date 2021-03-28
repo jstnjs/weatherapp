2f1b82eb0788e8b60b2bb635b31bb75efacfb728
Justin <33153367+jstnjs@users.noreply.github.com>
2021-03-28 15:00:41 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     weatherApiUrl: 'https://api.openweathermap.org/data/2.5/',
-    weatherApiKey: 'dbca293a1be0f045638b500869f34c98',
+    weatherApiKey: '1337',
 };
